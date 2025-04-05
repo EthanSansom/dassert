df6b0ae96b81c3c3ee086962116dd096e4c04c09
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-04-05 12:31:37 -0400

    Add utilities

--- R/utils-misc.R
+++ R/utils-misc.R
@@ -3,3 +3,15 @@
 map_chr <- function(.x, .f, ..., .nms = FALSE) {
   vapply(.x, .f, character(1L), ..., USE.NAMES = .nms)
 }
+
+map_lgl <- function(.x, .f, ..., .nms = FALSE) {
+  vapply(.x, .f, logical(1L), ..., USE.NAMES = .nms)
+}
+
+is_assertion_fail <- function(x) {
+  anyNA(x) || !all(x)
+}
+
+is_assertion_pass <- function(x) {
+  !anyNA(x) && all(x)
+}
