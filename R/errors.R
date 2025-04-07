15739e77a03b90e5dfcc36f8ab60b42172364cd0
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-04-07 09:43:47 -0400

    Add helpers for validating user input

--- R/errors.R
+++ R/errors.R
@@ -1,3 +1,5 @@
+# check ------------------------------------------------------------------------
+
 check_is_type <- function(
     x,
     is_type,
@@ -9,9 +11,11 @@ check_is_type <- function(
   if ((is.null(x) && null_ok) || is_type(x)) {
     return(x)
   }
-  cli::cli_abort(
-    "{.arg {x_name}} must be {expected_type}, not {.obj_type_friendly(x)}.",
-    class = "dassert_error_input_type",
+  stop_input_type(
+    paste0(
+      or_null("{.arg {x_name}} must be {expected_type}", null_ok),
+      ", not {.obj_type_friendly(x)}."
+    ),
     call = call
   )
 }
@@ -26,9 +30,56 @@ check_is_class <- function(
   if (is_class(x)) {
     return(x)
   }
-  cli::cli_abort(
+  stop_input_type(
     "{.arg {x_name}} must be of class {.cls {expected_class}}, not {.cls {class(x)}}.",
-    class = "dassert_error_input_type",
     call = call
   )
 }
+
+check_unsupplied_null <- function(
+  x,
+  x_name = rlang::caller_arg(x),
+  call = rlang::caller_env()
+  ) {
+  if (is.null(x)) {
+    return(x)
+  }
+  stop_input_type(
+    "{.arg {x_name}} must not be supplied.",
+    call = call
+  )
+}
+
+# stop -------------------------------------------------------------------------
+
+stop_input_type <- function(message, call = rlang::caller_env()) {
+  cli::cli_abort(
+    message,
+    class = "dassert_error_input_type",
+    call = call,
+    .envir = rlang::caller_env()
+  )
+}
+
+stop_input_incompatible <- function(message, call = rlang::caller_env()) {
+  cli::cli_abort(
+    message,
+    class = "dassert_error_input_incompatible",
+    call = call,
+    .envir = rlang::caller_env()
+  )
+}
+
+stop_internal <- function(message, call = rlang::caller_env()) {
+  cli::cli_abort(
+    message,
+    class = "dassert_error_internal",
+    call = call,
+    .envir = rlang::caller_env(),
+    .internal = TRUE
+  )
+}
+
+# helpers ----------------------------------------------------------------------
+
+
